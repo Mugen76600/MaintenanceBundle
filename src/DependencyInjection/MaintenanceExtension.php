835042b170f27b2c06df6c403c0f84bffadbd803
Mugen76600 <55193203+Mugen76600@users.noreply.github.com>
2026-04-29 15:40:11 +0200

    Update MaintenanceExtension.php

--- src/DependencyInjection/MaintenanceExtension.php
+++ src/DependencyInjection/MaintenanceExtension.php
@@ -3,7 +3,7 @@ namespace BretRZaun\MaintenanceBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 /**
